5c29136ced96d1fcbd5aa329b765ce667e626362
Di0118 <dianaeli1830@gmail.com>
2026-05-31 17:18:33 -0500

    archivo _redirects incluido

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
@@ -10,4 +9,11 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  publicDir: 'public',
+  build: {
+    rollupOptions: {
+      
+      publicDir: 'public' 
+    }
+  }
 })
