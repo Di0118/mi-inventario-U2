a8b2344ffbff17c0a4df8e7068434b2f2814d7a8
Di0118 <dianaeli1830@gmail.com>
2026-05-30 21:32:20 -0500

    corrigiendo

--- frontend/src/composables/useApi.js
+++ frontend/src/composables/useApi.js
@@ -18,7 +18,7 @@ export function useApi(){
     : `${baseUrl.replace(/\/$/, '')}/${url.replace(/^\//, '')}`;
 
        try {
-        console.log("URGENTE: La URL que estoy llamando es:", urlCompleta);
+        console.log("URGENTE: La URL que estoy llamando es:", urlCompleta);gi
             const response = await axios({
                 url: urlCompleta,
                 ...options
